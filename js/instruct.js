e08a4aa5da99586c0c5bde095a77886d9ab082d4
Tammy <sylvestershillocheori@gmail.com>
2026-08-23 20:25:48 +0100

    rearranged files

--- js/instruct.js
+++ js/instruct.js
@@ -1,2 +1,26 @@
     function myFunction(alert)
     function alert(message?: any): void;
+
+
+
+
+
+
+
+
+
+
+// Redirect //
+
+       const form = document.querySelector("form");
+
+    form.addEventListener("submit", function (e) {
+      e.preventDefault();
+
+      alert("Login Successful!");
+
+      // Redirect after 1 second
+      setTimeout(() => {
+        window.location.href = "/page/login.html ";
+      }, 1000);
+    });
